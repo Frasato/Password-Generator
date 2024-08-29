9650ea24e6ba060f2da87a01aa4a2fa59a59f275
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-29 11:56:33 -0300

    Add CrossOrigins

--- api/src/main/java/com/passwordgen/api/controllers/PasswordGenerationController.java
+++ api/src/main/java/com/passwordgen/api/controllers/PasswordGenerationController.java
@@ -2,12 +2,14 @@ package com.passwordgen.api.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Random;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class PasswordGenerationController {
 
     Random random = new Random();
